ce949b7d59d885dec99722e3affc97ae5f4ef02c
Bùi Lương Cao Sơn <builuongcaoson@gmail.com>
2023-04-13 11:54:39 +0700

    Update

--- app/src/main/java/com/sola/anime/ai/generator/feature/main/MainActivity.kt
+++ app/src/main/java/com/sola/anime/ai/generator/feature/main/MainActivity.kt
@@ -1,30 +1,35 @@
 package com.sola.anime.ai.generator.feature.main
 
+import android.animation.ValueAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.View
 import com.basic.common.base.LsActivity
 import com.basic.common.base.LsPageAdapter
+import com.basic.common.extension.getDimens
 import com.basic.common.extension.lightStatusBar
 import com.basic.common.extension.transparent
-import com.sola.anime.ai.generator.R
+import com.jakewharton.rxbinding2.view.clicks
 import com.sola.anime.ai.generator.databinding.ActivityMainBinding
+import com.sola.anime.ai.generator.databinding.LayoutBottomMainBinding
 import com.sola.anime.ai.generator.feature.main.image.ImageFragment
 import com.sola.anime.ai.generator.feature.main.setting.SettingFragment
 import com.sola.anime.ai.generator.feature.main.text.TextFragment
+import com.uber.autodispose.android.lifecycle.scope
+import com.uber.autodispose.autoDispose
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : LsActivity() {
 
-    private val fragments by lazy {
-        listOf(
-            TextFragment(),
-            ImageFragment(),
-            SettingFragment()
-        )
-    }
-
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val fragments by lazy { listOf(TextFragment(), ImageFragment(), SettingFragment()) }
+    private val bottomTabs by lazy { binding.initTabBottom() }
+    private val subjectTabClicks: Subject<Int> = BehaviorSubject.createDefault(0) // Default tab home
+    private var tabIndex = 0 // Default tab home
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +38,49 @@ class MainActivity : LsActivity() {
         setContentView(binding.root)
 
         initView()
+        initObservable()
+    }
+
+    private fun initObservable() {
+        bottomTabs.forEachIndexed { index, tab ->
+            tab
+                .viewClicks
+                .clicks()
+                .autoDispose(scope())
+                .subscribe { subjectTabClicks.onNext(index) }
+        }
+
+        subjectTabClicks
+            .take(1)
+            .delay(250, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .autoDispose(scope())
+            .subscribe { index ->
+                scrollToPage(index)
+            }
+
+        subjectTabClicks
+            .skip(1)
+            .distinctUntilChanged()
+            .autoDispose(scope())
+            .subscribe { index ->
+                scrollToPage(index)
+            }
+    }
+
+    private fun scrollToPage(index: Int) {
+        if (tabIndex != index){
+            bottomTabs.getOrNull(tabIndex)?.viewHide?.animShowTop()
+            bottomTabs.getOrNull(tabIndex)?.viewShow?.animHideBottom()
+        }
+
+        bottomTabs.getOrNull(index)?.viewHide?.animHideTop()
+        bottomTabs.getOrNull(index)?.viewShow?.animShowBottom()
+        bottomTabs.getOrNull(index)?.let { binding.viewBottom.viewDividerTab.animToCenterView(it.viewClicks, it.viewShow.width) }
+
+        binding.viewPager.currentItem = index
+        tabIndex = index
     }
 
     private fun initView() {
@@ -42,6 +90,37 @@ class MainActivity : LsActivity() {
             }
             this.offscreenPageLimit = this.adapter?.count ?: 0
         }
+        binding.viewBottom.viewTab1.setOnClickListener {
+            binding.viewBottom.imageTab1.animate().translationY(-binding.viewBottom.imageTab1.height.toFloat()).alpha(0f).setDuration(250).start()
+            binding.viewBottom.textTab1.animate().translationY(0f).alpha(1f).setDuration(250).start()
+        }
+    }
+
+    private data class Tab(val viewClicks: View, val viewHide: View, val viewShow: View)
+    private fun ActivityMainBinding.initTabBottom() = listOf(viewBottom.initTabHome(), viewBottom.initTabExplore(), viewBottom.initTabGallery(), viewBottom.initTabSetting())
+    private fun LayoutBottomMainBinding.initTabHome() = Tab(viewTab1, imageTab1, textTab1)
+    private fun LayoutBottomMainBinding.initTabExplore() = Tab(viewTab2, imageTab2, textTab2)
+    private fun LayoutBottomMainBinding.initTabGallery() = Tab(viewTab3, imageTab3, textTab3)
+    private fun LayoutBottomMainBinding.initTabSetting() = Tab(viewTab4, imageTab4, textTab4)
+    private fun View.animHideTop() { animate().translationY(-getDimens(com.intuit.sdp.R.dimen._30sdp)).alpha(0f).setDuration(100).start() }
+    private fun View.animShowTop() { animate().translationY(0f).alpha(1f).setDuration(100).start() }
+    private fun View.animHideBottom() { animate().translationY(getDimens(com.intuit.sdp.R.dimen._30sdp)).alpha(0f).setDuration(100).start() }
+    private fun View.animShowBottom() { animate().translationY(0f).alpha(1f).setDuration(100).start() }
+
+    private fun View.animToCenterView(centerView: View, newWidth: Int) {
+        val newLayoutParams = this.layoutParams
+        ValueAnimator.ofInt(width, newWidth).apply {
+            this.duration = 100
+            this.addUpdateListener {
+                val value = it.animatedValue as Int
+                newLayoutParams.width = value
+                this@animToCenterView.layoutParams = newLayoutParams
+            }
+            this.start()
+        }
+
+        val endX = centerView.x + centerView.width / 2 - newWidth / 2
+        animate().translationX(endX).setDuration(100).start()
     }
 
 }
