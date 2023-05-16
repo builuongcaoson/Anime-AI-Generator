e5773fa21b6420fb472da32a909ff5d9c02e8a03
Bùi Lương Cao Sơn <builuongcaoson@gmail.com>
2023-05-16 16:07:19 +0700

    Update

--- app/src/main/java/com/sola/anime/ai/generator/domain/model/config/tutorial/Tutorial.kt
+++ app/src/main/java/com/sola/anime/ai/generator/domain/model/config/tutorial/Tutorial.kt
@@ -5,72 +5,104 @@ import com.sola.anime.ai.generator.R
 
 enum class TutorialStep(val display: String, @DrawableRes val preview: Int, val childs: List<TutorialStep2>){
     Princess(
-        display = "Princess",
-        preview = R.drawable.preview_tutorial_princess_1,
+        display = "Angel",
+        preview = R.drawable.preview_tutorial_angel,
         childs = listOf(
             TutorialStep2(
+                display = "Water magic",
+                preview = R.drawable.preview_tutorial_angel_water_magic,
+                childs = listOf(
+                    TutorialStep3(
+                        display = "Short hair",
+                        preview = R.drawable.preview_tutorial_angel_water_magic_short_hair
+                    ),
+                    TutorialStep3(
+                        display = "Blue hair",
+                        preview = R.drawable.preview_tutorial_angel_water_magic_blue_hair
+                    )
+                )
+            ),
+            TutorialStep2(
+                display = "Wings white blood",
+                preview = R.drawable.preview_tutorial_angel_wings_white_blood,
+                childs = listOf(
+                    TutorialStep3(
+                        display = "Golden hair",
+                        preview = R.drawable.preview_tutorial_angel_wings_white_blood_golden_hair
+                    ),
+                    TutorialStep3(
+                        display = "Red hair",
+                        preview = R.drawable.preview_tutorial_angel_wings_white_blood_red_hair
+                    ),
+                    TutorialStep3(
+                        display = "Short hair",
+                        preview = R.drawable.preview_tutorial_angel_wings_white_blood_short_hair
+                    )
+                )
+            ),
+            TutorialStep2(
                 display = "Red hair",
-                preview = R.drawable.preview_tutorial_princess_red_hair_1,
+                preview = R.drawable.preview_tutorial_angel_red_hair,
                 childs = listOf(
                     TutorialStep3(
                         display = "Short hair",
-                        preview = R.drawable.preview_tutorial_princess_red_hair_short_hair_1
-                    )
-                )
-            ),
-            TutorialStep2(
-                display = "Blue hair",
-                preview = R.drawable.preview_tutorial_princess_blue_hair_1,
-                childs = listOf(
+                        preview = R.drawable.preview_tutorial_angel_red_hair_short_hair
+                    ),
                     TutorialStep3(
-                        display = "Short hair",
-                        preview = R.drawable.preview_tutorial_princess_blue_hair_short_hair_1
-                    )
-                )
-            ),
-            TutorialStep2(
-                display = "White hair",
-                preview = R.drawable.preview_tutorial_princess_white_hair_1,
-                childs = listOf(
-                    TutorialStep3(
-                        display = "Short hair",
-                        preview = R.drawable.preview_tutorial_princess_blue_hair_short_hair_1
+                        display = "White dress",
+                        preview = R.drawable.preview_tutorial_angel_red_hair_white_dress
                     )
                 )
             )
         )
     ),
-    CoolBoy(
-        display = "Cool Boy",
-        preview = R.drawable.preview_tutorial_cool_boy_1,
+    YoungBoy(
+        display = "Young Boy",
+        preview = R.drawable.preview_tutorial_young_boy,
         childs = listOf(
             TutorialStep2(
-                display = "Chinese clothes",
-                preview = R.drawable.preview_tutorial_cool_boy_chinese_clothes_1,
+                display = "Wear a hat",
+                preview = R.drawable.preview_tutorial_young_boy_wear_hat,
                 childs = listOf(
                     TutorialStep3(
-                        display = "Short hair",
-                        preview = R.drawable.preview_tutorial_princess_red_hair_short_hair_1
+                        display = "Yellow magic",
+                        preview = R.drawable.preview_tutorial_young_boy_wear_hat_yellow_magic
+                    ),
+                    TutorialStep3(
+                        display = "Fire magic",
+                        preview = R.drawable.preview_tutorial_young_boy_wear_hat_red_magic
+                    ),
+                    TutorialStep3(
+                        display = "Holding the scepter",
+                        preview = R.drawable.preview_tutorial_young_boy_wear_hat_holding_scepter
                     )
                 )
             ),
             TutorialStep2(
-                display = "Green hair",
-                preview = R.drawable.preview_tutorial_cool_boy_green_hair_1,
+                display = "Red wand",
+                preview = R.drawable.preview_tutorial_young_boy_red_wand,
                 childs = listOf(
                     TutorialStep3(
-                        display = "Short hair",
-                        preview = R.drawable.preview_tutorial_princess_blue_hair_short_hair_1
+                        display = "Red hair",
+                        preview = R.drawable.preview_tutorial_young_boy_red_wand_red_hair
+                    ),
+                    TutorialStep3(
+                        display = "Wear aristocratic clothes",
+                        preview = R.drawable.preview_tutorial_young_boy_red_wand_aristocratic_clothes
                     )
                 )
             ),
             TutorialStep2(
-                display = "Red eyes",
-                preview = R.drawable.preview_tutorial_cool_boy_red_eyes_1,
+                display = "White robe",
+                preview = R.drawable.preview_tutorial_young_boy_white_robe,
                 childs = listOf(
                     TutorialStep3(
-                        display = "Short hair",
-                        preview = R.drawable.preview_tutorial_princess_blue_hair_short_hair_1
+                        display = "Wings white blood",
+                        preview = R.drawable.preview_tutorial_young_boy_white_hair_wings_blood
+                    ),
+                    TutorialStep3(
+                        display = "Wings black blood",
+                        preview = R.drawable.preview_tutorial_young_boy_white_hair_wings_black_blood
                     )
                 )
             )
