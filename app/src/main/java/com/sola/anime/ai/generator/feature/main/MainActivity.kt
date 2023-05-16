b849c7394471a297c01dcaf56a107497193888c2
builuongcaoson <builuongcaoson@gmail.com>
2023-05-16 22:14:06 +0700

    Update

--- app/src/main/java/com/sola/anime/ai/generator/feature/main/MainActivity.kt
+++ app/src/main/java/com/sola/anime/ai/generator/feature/main/MainActivity.kt
@@ -34,7 +34,8 @@ class MainActivity : LsActivity() {
     @Inject lateinit var prefs: Preferences
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val fragments by lazy { listOf(ArtFragment(), BatchFragment(), DiscoverFragment(), MineFragment()) }
+//    private val fragments by lazy { listOf(ArtFragment(), BatchFragment(), DiscoverFragment(), MineFragment()) }
+    private val fragments by lazy { listOf(ArtFragment(), MineFragment()) }
     private val bottomTabs by lazy { binding.initTabBottom() }
     private val subjectTabClicks: Subject<Int> = BehaviorSubject.createDefault(0) // Default tab home
     private var tabIndex = 0 // Default tab home
@@ -110,7 +111,8 @@ class MainActivity : LsActivity() {
     }
 
     private data class Tab(val viewClicks: View, val viewHide: View, val viewShow: View)
-    private fun ActivityMainBinding.initTabBottom() = listOf(viewBottom.initTabArt(), viewBottom.initTabBatch(), viewBottom.initTabDiscover(), viewBottom.initTabMine())
+//    private fun ActivityMainBinding.initTabBottom() = listOf(viewBottom.initTabArt(), viewBottom.initTabBatch(), viewBottom.initTabDiscover(), viewBottom.initTabMine())
+    private fun ActivityMainBinding.initTabBottom() = listOf(viewBottom.initTabArt(), viewBottom.initTabMine())
     private fun LayoutBottomMainBinding.initTabArt() = Tab(viewTab1, imageTab1, textTab1)
     private fun LayoutBottomMainBinding.initTabBatch() = Tab(viewTab2, imageTab2, textTab2)
     private fun LayoutBottomMainBinding.initTabDiscover() = Tab(viewTab3, imageTab3, textTab3)
