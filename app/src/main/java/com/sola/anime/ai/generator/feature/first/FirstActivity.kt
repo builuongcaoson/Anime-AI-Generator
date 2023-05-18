98c1b0049967cd83f794043837b53db98e426a06
builuongcaoson <builuongcaoson@gmail.com>
2023-05-18 23:11:39 +0700

    Update

--- app/src/main/java/com/sola/anime/ai/generator/feature/first/FirstActivity.kt
+++ app/src/main/java/com/sola/anime/ai/generator/feature/first/FirstActivity.kt
@@ -1,38 +1,24 @@
 package com.sola.anime.ai.generator.feature.first
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
-import android.graphics.Path
-import android.graphics.RectF
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
-import androidx.core.util.toRange
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.basic.common.base.LsActivity
 import com.basic.common.extension.clicks
 import com.basic.common.extension.lightStatusBar
 import com.basic.common.extension.transparent
-import com.basic.common.extension.tryOrNull
 import com.sola.anime.ai.generator.common.ConfigApp
 import com.sola.anime.ai.generator.common.Navigator
 import com.sola.anime.ai.generator.common.extension.back
 import com.sola.anime.ai.generator.common.extension.makeLinks
 import com.sola.anime.ai.generator.common.extension.startMain
 import com.sola.anime.ai.generator.common.extension.startTutorial
-import com.sola.anime.ai.generator.common.util.AutoScrollLayoutManager
 import com.sola.anime.ai.generator.data.Preferences
 import com.sola.anime.ai.generator.databinding.ActivityFirstBinding
 import com.sola.anime.ai.generator.feature.first.adapter.PreviewAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
-import kotlin.math.sin
 
 @AndroidEntryPoint
 class FirstActivity : LsActivity() {
@@ -69,69 +55,69 @@ class FirstActivity : LsActivity() {
     }
 
     private fun initData() {
-        val dataAfterChunked = configApp.firstPreviews.chunked(configApp.firstPreviews.size / 2)
+//        val dataAfterChunked = configApp.firstPreviews.chunked(configApp.firstPreviews.size / 2)
 
-        previewAdapter1.let { adapter ->
-            adapter.data = dataAfterChunked.getOrNull(0) ?: listOf()
-            adapter.totalCount = adapter.data.size
-            binding.recyclerView1.apply {
-                this.post { this.smoothScrollToPosition(adapter.data.size - 1) }
-            }
-        }
-
-        previewAdapter2.let { adapter ->
-            adapter.data = dataAfterChunked.getOrNull(1) ?: listOf()
-            adapter.totalCount = adapter.data.size
-            binding.recyclerView2.apply {
-                this.post { this.smoothScrollToPosition(adapter.data.size - 1) }
-            }
-        }
+//        previewAdapter1.let { adapter ->
+//            adapter.data = dataAfterChunked.getOrNull(0) ?: listOf()
+//            adapter.totalCount = adapter.data.size
+//            binding.recyclerView1.apply {
+//                this.post { this.smoothScrollToPosition(adapter.data.size - 1) }
+//            }
+//        }
+//
+//        previewAdapter2.let { adapter ->
+//            adapter.data = dataAfterChunked.getOrNull(1) ?: listOf()
+//            adapter.totalCount = adapter.data.size
+//            binding.recyclerView2.apply {
+//                this.post { this.smoothScrollToPosition(adapter.data.size - 1) }
+//            }
+//        }
     }
 
-    override fun onResume() {
-        registerScrollListener()
-        super.onResume()
-    }
+//    override fun onResume() {
+//        registerScrollListener()
+//        super.onResume()
+//    }
+//
+//    override fun onDestroy() {
+//        unregisterScrollListener()
+//        super.onDestroy()
+//    }
 
-    override fun onDestroy() {
-        unregisterScrollListener()
-        super.onDestroy()
-    }
+//    private val scrollListener = object: RecyclerView.OnScrollListener() {
+//        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//            tryOrNull {
+//                val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return@tryOrNull
+//
+//                val visibleItemCount = layoutManager.childCount
+//                val totalItemCount = layoutManager.itemCount
+//                val pastVisibleItems = layoutManager.findLastVisibleItemPosition()
+//
+//                tryOrNull {
+//                    if (pastVisibleItems + visibleItemCount >= totalItemCount - 2) {
+//                        when (recyclerView) {
+//                            binding.recyclerView1 -> {
+//                                tryOrNull { recyclerView.post { previewAdapter1.insert() } }
+//                            }
+//                            binding.recyclerView2 -> {
+//                                tryOrNull { recyclerView.post { previewAdapter2.insert() } }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
 
-    private val scrollListener = object: RecyclerView.OnScrollListener() {
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            tryOrNull {
-                val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return@tryOrNull
-
-                val visibleItemCount = layoutManager.childCount
-                val totalItemCount = layoutManager.itemCount
-                val pastVisibleItems = layoutManager.findLastVisibleItemPosition()
-
-                tryOrNull {
-                    if (pastVisibleItems + visibleItemCount >= totalItemCount - 2) {
-                        when (recyclerView) {
-                            binding.recyclerView1 -> {
-                                tryOrNull { recyclerView.post { previewAdapter1.insert() } }
-                            }
-                            binding.recyclerView2 -> {
-                                tryOrNull { recyclerView.post { previewAdapter2.insert() } }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private fun registerScrollListener(){
-        binding.recyclerView1.addOnScrollListener(scrollListener)
-        binding.recyclerView2.addOnScrollListener(scrollListener)
-    }
-
-    private fun unregisterScrollListener(){
-        binding.recyclerView1.removeOnScrollListener(scrollListener)
-        binding.recyclerView2.removeOnScrollListener(scrollListener)
-    }
+//    private fun registerScrollListener(){
+//        binding.recyclerView1.addOnScrollListener(scrollListener)
+//        binding.recyclerView2.addOnScrollListener(scrollListener)
+//    }
+//
+//    private fun unregisterScrollListener(){
+//        binding.recyclerView1.removeOnScrollListener(scrollListener)
+//        binding.recyclerView2.removeOnScrollListener(scrollListener)
+//    }
 
     private fun initObservable() {
 
@@ -146,37 +132,51 @@ class FirstActivity : LsActivity() {
                 navigator.showTerms()
             }
         )
-        binding.recyclerView1.apply {
-            this.layoutManager = AutoScrollLayoutManager(this@FirstActivity).apply {
-                this.orientation = LinearLayoutManager.VERTICAL
-            }
-            this.adapter = previewAdapter1
-        }
-        binding.recyclerView2.apply {
-            this.layoutManager =  AutoScrollLayoutManager(this@FirstActivity).apply {
-                this.orientation = LinearLayoutManager.VERTICAL
-                this.reverseLayout = true
-            }
-            this.adapter = previewAdapter2
-        }
+//        binding.recyclerView1.apply {
+//            this.layoutManager = AutoScrollLayoutManager(this@FirstActivity).apply {
+//                this.orientation = LinearLayoutManager.VERTICAL
+//            }
+//            this.adapter = previewAdapter1
+//        }
+//        binding.recyclerView2.apply {
+//            this.layoutManager =  AutoScrollLayoutManager(this@FirstActivity).apply {
+//                this.orientation = LinearLayoutManager.VERTICAL
+//                this.reverseLayout = true
+//            }
+//            this.adapter = previewAdapter2
+//        }
 
-        binding.viewAnim1.animBubble()
-        binding.viewAnim2.animBubble()
+        listOf(
+            binding.viewAnim1,
+            binding.viewAnim2,
+            binding.viewAnim3,
+            binding.viewAnim4,
+            binding.viewAnim5,
+            binding.viewAnim6,
+            binding.viewAnim7,
+            binding.viewAnim8
+        ).forEachIndexed { index, view ->
+            view.animScale(index)
+        }
     }
 
-    private fun View.animBubble(to: Float = (-40..40).random().toFloat()) {
-        val animation = ObjectAnimator.ofFloat(0f, to)
+    private fun View.animScale(index: Int) {
+        val animation = ObjectAnimator.ofFloat(0.95f, 1.05f)
         animation.apply {
             repeatCount = ObjectAnimator.INFINITE
             repeatMode = ObjectAnimator.REVERSE
             duration = 1000
             interpolator = AccelerateDecelerateInterpolator()
+            startDelay = if (index % 2 == 0) 250 else 0
             addUpdateListener {
                 val animatedValue = it.animatedValue as Float
 
-                this@animBubble.translationX = animatedValue
-                this@animBubble.translationY = animatedValue
-                this@animBubble.rotation = animatedValue / 10
+//                this@animBubble.translationX = animatedValue
+//                this@animBubble.translationY = animatedValue
+//                this@animBubble.rotation = animatedValue / 10
+
+                this@animScale.scaleX = animatedValue
+                this@animScale.scaleY = animatedValue
             }
         }
         animation.start()
