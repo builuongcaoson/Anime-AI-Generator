2afb6412bc8730bca4cc79012359484e399d076d
unknown <sola.luthimylanh@gmail.com>
2023-06-28 21:35:22 +0700

    Update

--- app/src/main/java/com/sola/anime/ai/generator/feature/iap/adapter/PreviewAdapter.kt
+++ app/src/main/java/com/sola/anime/ai/generator/feature/iap/adapter/PreviewAdapter.kt
@@ -37,40 +37,40 @@ class PreviewAdapter @Inject constructor(): LsAdapter<IAP, ItemPreviewIapBinding
             .with(context)
             .load(item.preview)
             .error(R.drawable.place_holder_image)
-            .placeholder(R.drawable.place_holder_image)
+//            .placeholder(R.drawable.place_holder_image)
             .transition(DrawableTransitionOptions.withCrossFade())
-            .listener(object: RequestListener<Drawable>{
-                override fun onLoadFailed(
-                    e: GlideException?,
-                    model: Any?,
-                    target: Target<Drawable>?,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    binding.viewPreview.cardElevation = 0f
-                    binding.preview.setImageResource(R.drawable.place_holder_image)
-                    return false
-                }
-
-                override fun onResourceReady(
-                    resource: Drawable?,
-                    model: Any?,
-                    target: Target<Drawable>?,
-                    dataSource: DataSource?,
-                    isFirstResource: Boolean
-                ): Boolean {
-                    resource?.let {
-                        binding.viewPreview.cardElevation = context.getDimens(com.intuit.sdp.R.dimen._2sdp)
-                        binding.preview.setImageDrawable(resource)
-                        binding.preview.animate().alpha(1f).setDuration(100).start()
-                    } ?: run {
-                        binding.viewPreview.cardElevation = 0f
-                        binding.preview.setImageResource(R.drawable.place_holder_image)
-                        binding.preview.animate().alpha(0f).setDuration(100).start()
-                    }
-                    return false
-                }
-
-            })
+//            .listener(object: RequestListener<Drawable>{
+//                override fun onLoadFailed(
+//                    e: GlideException?,
+//                    model: Any?,
+//                    target: Target<Drawable>?,
+//                    isFirstResource: Boolean
+//                ): Boolean {
+//                    binding.viewPreview.cardElevation = 0f
+//                    binding.preview.setImageResource(R.drawable.place_holder_image)
+//                    return false
+//                }
+//
+//                override fun onResourceReady(
+//                    resource: Drawable?,
+//                    model: Any?,
+//                    target: Target<Drawable>?,
+//                    dataSource: DataSource?,
+//                    isFirstResource: Boolean
+//                ): Boolean {
+//                    resource?.let {
+//                        binding.viewPreview.cardElevation = context.getDimens(com.intuit.sdp.R.dimen._2sdp)
+//                        binding.preview.setImageDrawable(resource)
+//                        binding.preview.animate().alpha(1f).setDuration(100).start()
+//                    } ?: run {
+//                        binding.viewPreview.cardElevation = 0f
+//                        binding.preview.setImageResource(R.drawable.place_holder_image)
+//                        binding.preview.animate().alpha(0f).setDuration(100).start()
+//                    }
+//                    return false
+//                }
+//
+//            })
             .into(binding.preview)
     }
 
